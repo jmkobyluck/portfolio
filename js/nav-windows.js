1c291c050b266bc656e118d1a936302c70dbcf82
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-04-01 07:10:22 -0400

    Revert "Updated files"
    This reverts commit 01fc0b5051ef9dc527fc32af3412bf5d379b1149.

--- js/nav-windows.js
+++ js/nav-windows.js
@@ -1,13 +1,9 @@
 function OnShowPart(e, part) {
-    let width = window.innerWidth;
-
-console.log(width);
-
     let elements = {
         main: document.getElementById('main'),
         left: document.getElementById('left'),
         right: document.getElementById('right'),
-        bottom: document.getElementById('bottom'),
+        bottom: document.getElementById('bottom')
     }
 
     let isAboutToShow = !!!elements[part].style.transform;
@@ -23,12 +19,8 @@ console.log(width);
             elements.bottom.style.transform = null;
             if (!!elements.left.style.transform)
                 elements.left.style.transform = null;
-            else {
-                if(width < 600)
-                    elements.left.style.transform = "translateY(100%)";
-                else
-                    elements.left.style.transform = "translateX(958px)";
-            }
+            else
+                elements.left.style.transform = "translateX(958px)";
             break;
         case 'right':
             elements.left.style.transform = null;
@@ -36,24 +28,15 @@ console.log(width);
             if (!!elements.right.style.transform)
                 elements.right.style.transform = null;
             else
-            {
-                if(width < 600)
-                    elements.right.style.transform = "translateY(100%)";
-                else
-                    elements.right.style.transform = "translateX(-962px)";
-            }
+                elements.right.style.transform = "translateX(-962px)";
             break;
         case 'bottom':
             elements.right.style.transform = null;
             elements.left.style.transform = null;
             if (!!elements.bottom.style.transform)
                 elements.bottom.style.transform = null;
-            else{
-                if(width < 600)
-                    elements.bottom.style.transform = "translateY(100%)";
-                else 
-                    elements.bottom.style.transform = "translateY(-100%)";
-            }
+            else
+                elements.bottom.style.transform = "translateY(-700px)";
             break;
         default:
             break;

91f73712320242dd1aec23170e0318c23e4e8bd9
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-08-01 10:42:38 -0400

    Updated bottom menu height to allow for more presentation slots

--- js/nav-windows.js
+++ js/nav-windows.js
@@ -1,44 +1,38 @@
 function OnShowPart(e, part) {
-    let elements = {
-        main: document.getElementById('main'),
-        left: document.getElementById('left'),
-        right: document.getElementById('right'),
-        bottom: document.getElementById('bottom')
-    }
+  let elements = {
+    main: document.getElementById("main"),
+    left: document.getElementById("left"),
+    right: document.getElementById("right"),
+    bottom: document.getElementById("bottom"),
+  };
 
-    let isAboutToShow = !!!elements[part].style.transform;
+  let isAboutToShow = !!!elements[part].style.transform;
 
-    if (!isAboutToShow)
-        elements.main.style.filter = null;
-    else
-        elements.main.style.filter = "blur(20px)";
+  if (!isAboutToShow) elements.main.style.filter = null;
+  else elements.main.style.filter = "blur(20px)";
 
-    switch (part) {
-        case 'left':
-            elements.right.style.transform = null;
-            elements.bottom.style.transform = null;
-            if (!!elements.left.style.transform)
-                elements.left.style.transform = null;
-            else
-                elements.left.style.transform = "translateX(958px)";
-            break;
-        case 'right':
-            elements.left.style.transform = null;
-            elements.bottom.style.transform = null;
-            if (!!elements.right.style.transform)
-                elements.right.style.transform = null;
-            else
-                elements.right.style.transform = "translateX(-962px)";
-            break;
-        case 'bottom':
-            elements.right.style.transform = null;
-            elements.left.style.transform = null;
-            if (!!elements.bottom.style.transform)
-                elements.bottom.style.transform = null;
-            else
-                elements.bottom.style.transform = "translateY(-700px)";
-            break;
-        default:
-            break;
-    }
+  switch (part) {
+    case "left":
+      elements.right.style.transform = null;
+      elements.bottom.style.transform = null;
+      if (!!elements.left.style.transform) elements.left.style.transform = null;
+      else elements.left.style.transform = "translateX(958px)";
+      break;
+    case "right":
+      elements.left.style.transform = null;
+      elements.bottom.style.transform = null;
+      if (!!elements.right.style.transform)
+        elements.right.style.transform = null;
+      else elements.right.style.transform = "translateX(-962px)";
+      break;
+    case "bottom":
+      elements.right.style.transform = null;
+      elements.left.style.transform = null;
+      if (!!elements.bottom.style.transform)
+        elements.bottom.style.transform = null;
+      else elements.bottom.style.transform = "translateY(-800px)";
+      break;
+    default:
+      break;
+  }
 }
